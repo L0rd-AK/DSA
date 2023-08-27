915f3b213b9f9b6f843627359cb31727a0d50075
Amit Kumar <aka883207@gmail.com>
2023-08-27 22:35:31 +0600

    27/08/23

--- Number_Theory/Nth-permutation_of_a_string.cpp
+++ Number_Theory/Nth-permutation_of_a_string.cpp
@@ -1,27 +1,24 @@
-
 #include <bits/stdc++.h> 
+
 using namespace std; 
 #define ll long long
+
+string s; 
 
 string permute(int n,ll Nth) 
 {
 	string ans="";
 	Nth=Nth-1;
-    ll fact=1;
-    vector<ll> v;
-	for(int i=1;i<n;i++){
-        fact*=i;
-        v.push_back(i);
-    }
-    v.push_back(n);
+	ll fact=1;
+	for(int i=2;i<n;i++) fact*=i;
+	
 	while(1){
 		ll temp=Nth/fact;
-		ans+=to_string(v[temp]);
-		v.erase(v.begin() + temp);
-		if(v.size() == 0) break;
-
+		ans+=s[temp];
+		s.erase(s.begin() + temp);
+		if(s.size() == 0) break;
 		Nth=Nth%fact;
-		fact=fact/(v.size());
+		fact=fact/(s.size());
 	}
 	
 	return ans;
@@ -31,11 +28,15 @@ string permute(int n,ll Nth)
 int main() 
 { 
     freopen("input.txt","r",stdin);
-    int t,n;cin>>t;
+    int t;cin>>t;
     while(t--){
-        cin>>n;
+        cin>>s;
+        int n = s.length();
+
+		sort(s.begin(),s.end());
+		
         ll Nth;cin>>Nth; 
-	    cout<<permute(n,Nth)<<endl;
+	    cout<<permute(n,Nth+1)<<endl;
     } 
 	return 0; 
 } 
