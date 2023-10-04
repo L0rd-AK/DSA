046ed469b31e69cf1c5b5973c1f5b53f31f17572
Amit Kumar <aka883207@gmail.com>
2023-10-05 00:02:21 +0600

    05/10/23

--- Algorithms/DP_coin-change-minimum-number-of-ways.cpp
+++ Algorithms/DP_coin-change-minimum-number-of-ways.cpp
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int dp[1000][1000];
+long long dp[1000][1000]={0};
 int main() {
   freopen("D:\\VS-Code\\DSA\\input.txt","r",stdin);
   // freopen("D:\\VS-Code\\DSA\\output.txt","w",stdout);
-  for(int i=0;i<1000;i++)for(int j=0;j<1000;j++)dp[i][j]=INT_MAX-1;
   int n;
+  //enter number of coins Ex. n=3
   cin>>n;
   int coins[n];
+  // enter n coins Ex.{1,3,5}
   for(int i=0;i<n;i++)cin>>coins[i];
   int amount;
+  //enter amount
   cin>>amount;
-  for(int i=0;i<=n;i++) {dp[i][0]=0;}
-
+  for(int i=0;i<=n;i++) dp[i][0]=1;
   for(int i=1;i<=n;i++){
     for(int j=1;j<=amount;j++){
       if(coins[i-1]>amount) {dp[i][j]=dp[i-1][j];}
       else{
-        dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+        dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
       }
     }
-  }cout<<"Minimum number of coins: "<<dp[n][amount]<<endl;
+  }cout<<"Minimum number of ways: "<<dp[n][amount]<<endl;
   // for(int i=0;i<=n;i++){
   //   for(int j=0;j<=amount;j++){
   //     printf("(%d,%d)=>[%d] ",i,j,dp[i][j]);
