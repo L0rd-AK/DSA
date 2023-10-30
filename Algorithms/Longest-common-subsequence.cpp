33d8f6514fd941c805e6de9cf819370e1627ae25
Amit Kumar <aka883207@gmail.com>
2023-10-31 01:18:26 +0600

    31/10/23

--- Algorithms/Longest-common-subsequence.cpp
+++ Algorithms/Longest-common-subsequence.cpp
@@ -3,9 +3,28 @@ using namespace std;
 string a,b,s="";
 int LCS(int m,int n){
     if(m==0 || n==0) return 0;
-    if(a[m-1]==b[n-1]) return 1+LCS(m-1,n-1); 
+    if(a[m-1]==b[n-1]){
+        s+=b[n-1];
+        return 1+LCS(m-1,n-1);
+    }
     else return max(LCS(m-1,n),LCS(m,n-1));
 }
+string LCS_seq(int m,int n, string s){
+    if(m==0 || n==0){
+        reverse(s.begin(),s.end());
+        return s;
+    }
+    if(a[m-1]==b[n-1]){
+        s+=b[n-1];
+        return LCS_seq(m-1,n-1,s);
+    }
+    else{
+        string s1=LCS_seq(m-1,n,s);
+        string s2=LCS_seq(m,n-1,s);
+        return s1.length()>s2.length()?s1:s2;
+    }
+}
+
 int longestCommonSubsequence(string text1, string text2) {
     int dp[text1.length()+1][text2.length()+1];
     for(int i=text1.length();i>=0;i--){
@@ -16,7 +35,7 @@ int longestCommonSubsequence(string text1, string text2) {
     for(int i=text1.length()-1;i>=0;i--){
         for(int j=text2.length()-1;j>=0;j--){
             if(text1[i]==text2[j]){
-                dp[i][j]=1+dp[i+1][j+1];
+                dp[i][j]=dp[i+1][j+1]+1;//+1 means we found same char
             }else{
                 dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
             }
@@ -29,7 +48,8 @@ int main(){
     cin>>a>>b;
     int m=a.length();
     int n=b.length();
-    cout<<"Longest common subsequence length: "<<LCS(m,n)<<endl;
-    cout<<"Longest common subsequence length: "<<longestCommonSubsequence(a,b)<<endl;
+    cout<<"Longest common subsequence length: "<<LCS(m,n)<<endl;// recurtion
+    cout<<"Longest common subsequence: "<<LCS_seq(m,n,"")<<endl;// recurtion
+    cout<<"Longest common subsequence length: "<<longestCommonSubsequence(a,b)<<endl;// normal
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,62 +1,21 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// int dp[1000][1000];
-// int main() {
-//   freopen("D:\\VS-Code\\DSA\\input.txt","r",stdin);
-//   // freopen("D:\\VS-Code\\DSA\\output.txt","w",stdout);
-//   for(int i=0;i<1000;i++)for(int j=0;j<1000;j++)dp[i][j]=INT_MAX-1;
-//   int n;
-//   cin>>n;
-//   int coins[n];
-//   for(int i=0;i<n;i++)cin>>coins[i];
-//   int amount;
-//   cin>>amount;
-//   for(int i=0;i<=n;i++) {dp[i][0]=0;}
-
-//   for(int i=1;i<=n;i++){
-//     for(int j=1;j<=amount;j++){
-//       if(coins[i-1]>amount) {dp[i][j]=dp[i-1][j];}
-//       else{
-//         dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-//       }
-//     }
-//   }cout<<"Minimum number of coins: "<<dp[n][amount]<<endl;
-//   // for(int i=0;i<=n;i++){
-//   //   for(int j=0;j<=amount;j++){
-//   //     printf("(%d,%d)=>[%d] ",i,j,dp[i][j]);
-//   //   }cout<<endl;
-//   // }
- 
-
-//   return 0;
-// }
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int dp[1005][1005];
-int main() {
-     for(int i=0;i<1000;i++)for(int j=0;j<1000;j++)dp[i][j]=INT_MAX-1;
-     for(int i=0;i<1000;i++)for(int j=0;j<1000;j++)dp[i][0]=1;
-	int n,amount;
-	cin>>n;
-	while(n--){
-	    cin>>amount;
-	    if(amount%5==1 and amount%10==1){cout<<-1<<endl;continue;}
-	    int coins[]={5,10};
-	    for(int i=1;i<=2;i++){
-        for(int j=1;j<=amount;j++){
-          if(coins[i-1]>amount) {dp[i][j]=dp[i-1][j];}
-          else{
-            dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
-          }
-        }
-      }cout<<dp[2][amount]<<endl;
-      for(int i=0;i<=2;i++){
-        for(int j=0;j<=amount;j++){
-          printf("(%d,%d)=>[%d] ",i,j,dp[i][j]);
-        }cout<<endl;
+
+string lcs(string X, string Y, int m, int n, string s) {
+    if (m == 0 || n == 0)
+        return string(s.rbegin(), s.rend());
+    if (X[m-1] == Y[n-1])
+        return lcs(X, Y, m-1, n-1, s+X[m-1]);
+    else {
+        string s1 = lcs(X, Y, m-1, n, s);
+        string s2 = lcs(X, Y, m, n-1, s);
+        return (s1.size() > s2.size()) ? s1 : s2;
     }
-  
-  
-	}
-	return 0;
+}
+
+int main() {
+    string X = "amit";
+    string Y = "GXTXAYB";
+    cout << "LCS of " << X << " and " << Y << " is " << lcs(X , Y, X.size(), Y.size(), "");
+    return 0;
 }
