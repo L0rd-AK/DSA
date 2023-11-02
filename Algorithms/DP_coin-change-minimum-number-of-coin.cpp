ad272f5135544d58b235dd97f0d9a26f872a7c78
Amit Kumar <aka883207@gmail.com>
2023-11-02 12:06:44 +0600

    02/11/23

--- Algorithms/DP_coin-change-minimum-number-of-coin.cpp
+++ Algorithms/DP_coin-change-minimum-number-of-coin.cpp
@@ -15,7 +15,7 @@ int main() {
 
   for(int i=1;i<=n;i++){
     for(int j=1;j<=amount;j++){
-      if(coins[i-1]>amount) {dp[i][j]=dp[i-1][j];}
+      if(coins[i-1]>j) {dp[i][j]=dp[i-1][j];}
       else{
         dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
       }
--- Algorithms/DP_coin-change-minimum-number-of-ways.cpp
+++ Algorithms/DP_coin-change-minimum-number-of-ways.cpp
@@ -16,17 +16,12 @@ int main() {
   for(int i=0;i<=n;i++) dp[i][0]=1;
   for(int i=1;i<=n;i++){
     for(int j=1;j<=amount;j++){
-      if(coins[i-1]>amount) {dp[i][j]=dp[i-1][j];}
+      if(coins[i-1]>j) {dp[i][j]=dp[i-1][j];}
       else{
         dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
       }
     }
   }cout<<"Minimum number of ways: "<<dp[n][amount]<<endl;
-  // for(int i=0;i<=n;i++){
-  //   for(int j=0;j<=amount;j++){
-  //     printf("(%d,%d)=>[%d] ",i,j,dp[i][j]);
-  //   }cout<<endl;
-  // }
  
 
   return 0;
